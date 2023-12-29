f4ad208a110e80271b5371c07a7a964c711dd99a
Azri <muhammad.azri.f.s@gmail.com>
2023-12-29 22:24:27 +0700

    config(firebase):setup-firebase-project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:code_with_azri/firebase_options.dart';
 import 'package:code_with_azri/router/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -8,6 +10,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   EasyLocalization.logger.enableBuildModes = [];
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(MyApp(
     appRouter: AppRouter(),
