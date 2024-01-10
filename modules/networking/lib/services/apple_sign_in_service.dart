b347e286d4e52cd66176358e97df898dcd05a8ad
Azri <muhammad.azri.f.s@gmail.com>
2024-01-10 11:56:58 +0700

    fix(test):fix-auth-service-tests

---
+++ modules/networking/lib/services/apple_sign_in_service.dart
@@ -0,0 +1,14 @@
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+abstract class AppleSignInService {
+  Future<AuthorizationCredentialAppleID> getAppleIDCredential(
+      {required List<AppleIDAuthorizationScopes> scopes});
+}
+
+class AppleSignInServiceImpl implements AppleSignInService {
+  @override
+  Future<AuthorizationCredentialAppleID> getAppleIDCredential(
+      {required List<AppleIDAuthorizationScopes> scopes}) async {
+    return await SignInWithApple.getAppleIDCredential(scopes: scopes);
+  }
+}
--- modules/networking/test/auth_services_test.dart
+++ modules/networking/test/auth_services_test.dart
@@ -1,25 +1,78 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:models/dto/auth/sign_in_dto.dart';
 import 'package:models/dto/auth/sign_up_dto.dart';
+import 'package:networking/services/apple_sign_in_service.dart';
 import 'package:networking/services/auth_services.dart';
-
-class MockAuthServices extends Mock implements AuthService {}
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class MockUser extends Mock implements User {}
 
+class MockUserCredential extends Mock implements UserCredential {
+  @override
+  final User user;
+
+  MockUserCredential({required this.user});
+}
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
+
+class MockAppleSignInService extends Mock implements AppleSignInService {}
+
+class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {
+  @override
+  final Future<GoogleSignInAuthentication> authentication;
+
+  MockGoogleSignInAccount()
+      : authentication = Future.value(MockGoogleSignInAuthentication());
+}
+
+class MockGoogleSignInAuthentication extends Mock
+    implements GoogleSignInAuthentication {
+  @override
+  final String accessToken;
+  @override
+  final String idToken;
+
+  MockGoogleSignInAuthentication({
+    this.accessToken = 'token',
+    this.idToken = 'idToken',
+  });
+}
+
+class MockAuthorizationCredentialAppleID extends Mock
+    implements AuthorizationCredentialAppleID {}
+
 void main() {
   late AuthService mockAuthServices;
+  late FirebaseAuth mockFirebaseAuth;
+  late GoogleSignIn mockGoogleSignIn;
+  late AppleSignInService mockAppleSignInService;
   late GetIt getIt;
 
   setUp(() {
     getIt = GetIt.instance;
+    getIt.registerSingleton<FirebaseAuth>(MockFirebaseAuth());
+    getIt.registerSingleton<GoogleSignIn>(MockGoogleSignIn());
+    getIt.registerSingleton<AppleSignInService>(MockAppleSignInService());
 
-    getIt.registerSingleton<AuthService>(MockAuthServices());
+    getIt.registerFactory<AuthService>(
+      () => AuthService(
+        firebaseAuth: getIt.get<FirebaseAuth>(),
+        googleSignIn: getIt.get<GoogleSignIn>(),
+        appleSignInService: getIt.get<AppleSignInService>(),
+      ),
+    );
 
+    mockFirebaseAuth = getIt.get<FirebaseAuth>();
+    mockGoogleSignIn = getIt.get<GoogleSignIn>();
     mockAuthServices = getIt.get<AuthService>();
+    mockAppleSignInService = getIt.get<AppleSignInService>();
   });
 
   tearDown(() {
@@ -33,6 +86,11 @@ void main() {
     registerFallbackValue(
       const SignUpDTO(name: "name", email: "email", password: "password"),
     );
+    registerFallbackValue(MockUserCredential(user: MockUser()));
+    registerFallbackValue(const AuthCredential(
+      providerId: 'providerId',
+      signInMethod: 'signInMethod',
+    ));
   });
   group(
     "Auth Service Test",
@@ -43,20 +101,28 @@ void main() {
       const email = "email@domain.com";
 
       test("signInWithEmailAndPassword return User When Success", () async {
-        when(() => mockAuthServices.signInWithEmailAndPassword(any()))
-            .thenAnswer((_) async => MockUser());
+        when(() => mockFirebaseAuth.signInWithEmailAndPassword(
+              email: any(named: "email"),
+              password: any(named: 'password'),
+            )).thenAnswer(
+          (_) async => MockUserCredential(
+            user: MockUser(),
+          ),
+        );
 
         final user =
             await mockAuthServices.signInWithEmailAndPassword(signInDTO);
 
+        expect(user, isA<User>());
         expect(user, isNotNull);
-        expect(user, isA<MockUser>());
       });
 
       test("signInWithEmailAndPassword throws error when unsuccessful",
           () async {
-        when(() => mockAuthServices.signInWithEmailAndPassword(any()))
-            .thenThrow(
+        when(() => mockFirebaseAuth.signInWithEmailAndPassword(
+              email: any(named: "email"),
+              password: any(named: 'password'),
+            )).thenThrow(
           FirebaseAuthException(
             code: 'wrong-password',
             message:
@@ -67,14 +133,29 @@ void main() {
         callSignIn() async =>
             await mockAuthServices.signInWithEmailAndPassword(signInDTO);
 
-        expect(callSignIn, throwsA(isA<FirebaseAuthException>()));
+        expect(
+          callSignIn,
+          throwsA(
+            isA<String>().having(
+              (e) => e,
+              'message',
+              'The password is invalid or the user does not have a password.',
+            ),
+          ),
+        );
       });
 
       test(
         "signUp return User When Success",
         () async {
-          when(() => mockAuthServices.signUp(any()))
-              .thenAnswer((_) async => MockUser());
+          when(() => mockFirebaseAuth.createUserWithEmailAndPassword(
+                email: any(named: "email"),
+                password: any(named: 'password'),
+              )).thenAnswer(
+            (_) async => MockUserCredential(
+              user: MockUser(),
+            ),
+          );
 
           final user = await mockAuthServices.signUp(signUpDTO);
 
@@ -86,7 +167,10 @@ void main() {
       test(
         "signUp throws error when unsuccessful",
         () async {
-          when(() => mockAuthServices.signUp(any())).thenThrow(
+          when(() => mockFirebaseAuth.createUserWithEmailAndPassword(
+                email: any(named: "email"),
+                password: any(named: 'password'),
+              )).thenThrow(
             FirebaseAuthException(
               code: 'email-already-in-use',
               message: 'Email address is already in use by another account.',
@@ -95,15 +179,23 @@ void main() {
 
           callSignUp() async => await mockAuthServices.signUp(signUpDTO);
 
-          expect(callSignUp, throwsA(isA<FirebaseAuthException>()));
+          expect(
+              callSignUp,
+              throwsA(
+                isA<String>().having(
+                  (e) => e,
+                  'message',
+                  'Email address is already in use by another account.',
+                ),
+              ));
         },
       );
 
       test(
         "sendPasswordResetEmail completes successfully",
         () async {
-          when(() => mockAuthServices.sendPasswordResetEmail(email))
-              .thenAnswer((_) async {});
+          when(() => mockFirebaseAuth.sendPasswordResetEmail(
+              email: any(named: 'email'))).thenAnswer((_) async {});
 
           callSendPasswordResetEmail() async =>
               await mockAuthServices.sendPasswordResetEmail(email);
@@ -115,18 +207,28 @@ void main() {
       test(
         "sendPasswordResetEmail throws FirebaseAuthException for invalid email",
         () async {
-          when(() => mockAuthServices.sendPasswordResetEmail(email)).thenThrow(
-              FirebaseAuthException(
-                  code: 'user-not-found',
-                  message:
-                      'There is no user corresponding to the given email.'));
+          when(
+            () => mockFirebaseAuth.sendPasswordResetEmail(
+              email: any(
+                named: 'email',
+              ),
+            ),
+          ).thenThrow(FirebaseAuthException(
+              code: 'user-not-found',
+              message: 'There is no user corresponding to the given email.'));
 
           callSendPasswordResetEmail() async =>
               await mockAuthServices.sendPasswordResetEmail(email);
 
           expect(
             callSendPasswordResetEmail,
-            throwsA(isA<FirebaseAuthException>()),
+            throwsA(
+              isA<String>().having(
+                (e) => e,
+                'message',
+                'There is no user corresponding to the given email.',
+              ),
+            ),
           );
         },
       );
@@ -134,7 +236,7 @@ void main() {
       test(
         "signOut completes successfully",
         () async {
-          when(() => mockAuthServices.signOut()).thenAnswer((_) async {});
+          when(() => mockFirebaseAuth.signOut()).thenAnswer((_) async {});
 
           callSignOut() async => await mockAuthServices.signOut();
 
@@ -145,20 +247,37 @@ void main() {
       test(
         "signOut throws FirebaseAuthException when an error occurs",
         () async {
-          when(() => mockAuthServices.signOut()).thenThrow(
-              FirebaseAuthException(
-                  code: 'sign-out-error',
-                  message: 'An error occurred during sign-out.'));
+          when(() => mockFirebaseAuth.signOut()).thenThrow(
+            FirebaseAuthException(
+              code: 'sign-out-error',
+              message: 'An error occurred during sign-out.',
+            ),
+          );
 
           callSignOut() async => await mockAuthServices.signOut();
 
-          expect(callSignOut, throwsA(isA<FirebaseAuthException>()));
+          expect(
+            callSignOut,
+            throwsA(
+              isA<String>().having(
+                (e) => e,
+                'message',
+                'An error occurred during sign-out.',
+              ),
+            ),
+          );
         },
       );
 
       test("signInWithGoogle return User when SuccessFull", () async {
-        when(() => mockAuthServices.signInWithGoogle())
-            .thenAnswer((_) async => MockUser());
+        when(() => mockGoogleSignIn.signIn())
+            .thenAnswer((_) async => MockGoogleSignInAccount());
+
+        when(() => mockFirebaseAuth.signInWithCredential(any())).thenAnswer(
+          (_) async => MockUserCredential(
+            user: MockUser(),
+          ),
+        );
 
         final user = await mockAuthServices.signInWithGoogle();
 
@@ -168,20 +287,42 @@ void main() {
 
       test("signInWithGoogle throw FirebaseAuthException when UnsuccessFull",
           () async {
-        when(() => mockAuthServices.signInWithGoogle()).thenThrow(
+        when(() => mockGoogleSignIn.signIn())
+            .thenAnswer((_) async => MockGoogleSignInAccount());
+
+        when(() => mockFirebaseAuth.signInWithCredential(any())).thenThrow(
           FirebaseAuthException(
             code: 'sign_in_failed',
             message: 'Sign in failed',
           ),
         );
+
         callGoogleSignIn() async => await mockAuthServices.signInWithGoogle();
 
-        expect(callGoogleSignIn, throwsA(isA<FirebaseAuthException>()));
+        expect(
+          callGoogleSignIn,
+          throwsA(
+            isA<String>().having(
+              (e) => e,
+              'message',
+              'Sign in failed',
+            ),
+          ),
+        );
       });
 
       test("signInWithApple return User when SuccessFull", () async {
-        when(() => mockAuthServices.signInWithApple())
-            .thenAnswer((_) async => MockUser());
+        when(
+          () => mockAppleSignInService.getAppleIDCredential(
+            scopes: any(named: 'scopes'),
+          ),
+        ).thenAnswer((_) async => MockAuthorizationCredentialAppleID());
+
+        when(() => mockFirebaseAuth.signInWithCredential(any())).thenAnswer(
+          (_) async => MockUserCredential(
+            user: MockUser(),
+          ),
+        );
 
         final user = await mockAuthServices.signInWithApple();
 
@@ -191,22 +332,41 @@ void main() {
 
       test("signInWithApple throw FirebaseAuthException when UnsuccessFull",
           () async {
-        when(() => mockAuthServices.signInWithApple()).thenThrow(
+        when(
+          () => mockAppleSignInService.getAppleIDCredential(
+            scopes: any(named: 'scopes'),
+          ),
+        ).thenAnswer((_) async => MockAuthorizationCredentialAppleID());
+
+        when(() => mockFirebaseAuth.signInWithCredential(any())).thenThrow(
           FirebaseAuthException(
             code: 'sign_in_failed',
             message: 'Sign in failed',
           ),
         );
+
         callAppleSignIn() async => await mockAuthServices.signInWithApple();
 
-        expect(callAppleSignIn, throwsA(isA<FirebaseAuthException>()));
+        expect(
+          callAppleSignIn,
+          throwsA(
+            isA<String>().having(
+              (e) => e,
+              'message',
+              'Sign in failed',
+            ),
+          ),
+        );
       });
 
       test("authStateChanges emits user when signed in", () async {
         final mockUser = MockUser();
         final expectedUser = mockUser;
-        when(() => mockAuthServices.authStateChanges)
-            .thenAnswer((_) => Stream<User?>.value(mockUser));
+        final Stream<User?> iterableStream =
+            Stream<User?>.fromIterable([mockUser]);
+
+        when(mockFirebaseAuth.authStateChanges)
+            .thenAnswer((_) => iterableStream);
 
         final Stream<User?> stream = mockAuthServices.authStateChanges;
 
@@ -215,8 +375,10 @@ void main() {
       });
 
       test("authStateChanges emits null by default", () async {
-        when(() => mockAuthServices.authStateChanges)
-            .thenAnswer((_) => Stream<User?>.value(null));
+        final Stream<User?> iterableStream = Stream<User?>.fromIterable([null]);
+
+        when(mockFirebaseAuth.authStateChanges)
+            .thenAnswer((_) => iterableStream);
 
         final Stream<User?> stream = mockAuthServices.authStateChanges;
 
